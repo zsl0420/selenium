ab3490ec33f11cebd5d4c44591fb5e15a67b53d1
zsl <zsl19970420@163.com>
2021-02-06 14:57:03 +0800

    first selenium 脚本

---
+++ example/test_baidu.py
@@ -0,0 +1,16 @@
+#编写第一个自动化脚本文件
+
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+"""
+    用的是火狐浏览器
+"""
+driver.get("https://www.baidu.com")
+
+# 定位元素
+driver.find_element_by_id("kw").send_keys("Selenium")
+driver.find_element_by_id("su").click()
+
+#退出浏览器
+driver.quit()
